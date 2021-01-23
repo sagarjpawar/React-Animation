40bcb25af405f6fd8e411aadefcf0c35b9962d2c
Sagar <sagar.pawar@synerzip.com>
2021-01-23 18:31:06 +0530

    Animation with react-transition is done

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { Transition } from 'react-transition-group';
+
+import "./App.css";
+import Modal from "./components/Modal/Modal";
+import Backdrop from "./components/Backdrop/Backdrop";
+import List from "./components/List/List";
+
+class App extends Component {
+  state = {
+    modalIsOpen: false,
+    showBlock: false
+  }
+
+  showModal = () => {
+    this.setState({ modalIsOpen: true });
+  }
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>React Animations</h1>
+        <button className="Button" onClick={()=> this.setState({showBlock: !this.state.showBlock})}>Toggle</button>
+        <Transition in={this.state.showBlock} timeout={3000} 
+        mountOnEnter
+        unmountOnExit
+        onEnter={() => console.log('onEnter')}
+        onEntering={() => console.log('onEntering')}
+        onEntered={() => console.log('onEntered')}
+        onExit={() => console.log('onExit')}
+        onExiting={() => console.log('onExiting')}
+        onExited={() => console.log('onExited')}>
+          {state => (
+          <div style={{
+            backgroundColor: 'red',
+            height: 100,
+            width: 100,
+            margin: 'auto',
+            transition: 'opacity 1s ease-out',
+            opacity: state === 'exiting' ? 0 : 1
+          }}>{state}</div>
+          )}
+        </Transition>
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+        <Backdrop show={this.state.modalIsOpen} />
+        <button className="Button" onClick={this.showModal}>Open Modal</button>
+        <h3>Animating Lists</h3>
+        <List />
+      </div>
+    );
+  }
+}
+
+export default App;
